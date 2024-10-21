35fb2444cb9e0907a7d6dc9882b77aabf5a443eb
Soumen Mondal <93898777+msoumen097@users.noreply.github.com>
2024-10-21 19:04:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from FlipkartBot.retrieval_generation import generation
 from FlipkartBot.data_ingestion import data_ingestion
@@ -5,10 +6,8 @@ from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_groq import ChatGroq
 
-
 vstore = data_ingestion("done")
 chain = generation(vstore)
-
 
 app = Flask(__name__)
 
@@ -16,24 +15,22 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
-
-@app.route("/get", methods = ["POST", "GET"])
+@app.route("/get", methods=["POST", "GET"])
 def chat():
-   
-   if request.method == "POST":
-      msg = request.form["msg"]
-      input = msg
+    if request.method == "POST":
+        msg = request.form["msg"]
+        input = msg
 
-      result = chain.invoke(
-         {"input": input},
-    config={
-        "configurable": {"session_id": "soumen"}
-    },
-)["answer"]
+        result = chain.invoke(
+            {"input": input},
+            config={
+                "configurable": {"session_id": "soumen"}
+            },
+        )["answer"]
 
-      return str(result)
+        return str(result)
 
 if __name__ == '__main__':
-    
-    app.run(host="0.0.0.0", port=5000, debug= True)
+    # Get the port from the environment variable (default to 5000 if not set)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
